614188ad2a1bca37772d4e457082db2b7077bcc3
kalyanipatil <kalyani29patil@gmail.com>
2024-06-28 14:44:13 +0530

    v1:B32443452,SecondTest2

--- src/main/java/com/bikkadit/Njunit4/SecondTask.java
+++ src/main/java/com/bikkadit/Njunit4/SecondTask.java
@@ -2,6 +2,9 @@ package com.bikkadit.Njunit4;
 
 public class SecondTask {
 
+	public static void main(String[] args) {
+		
+	
 	
 	public void addition() {
 		
@@ -12,8 +15,22 @@ public class SecondTask {
 		
 		
 		System.out.println("addition of a and b="+c);
+	}
+		
+		
+		
+
+		public void substraction() {
+			
+			int a=10;
+			int b=20;
+			
+			int c=a-b;
+			
+			
+			System.out.println("substraction of a and b="+c);
 		
 		
 	}
 	
-}
+}}
