ec4682d0e637843d3f405e06aa7c77da2ac00f3d
kalyanipatil <kalyani29patil@gmail.com>
2024-06-28 16:08:40 +0530

    v1:B32454,demoConflictByDeveloper

--- src/main/java/com/bikkadit/Njunit4/Test.java
+++ src/main/java/com/bikkadit/Njunit4/Test.java
@@ -1,29 +1,30 @@
 package com.bikkadit.Njunit4;
 
 public class Test {
-  
-	//morning task
-	public String demo(){
-		
+
+	// morning task
+	public String demo() {
+
 		return "welcome";
 	}
-	
-	
-	
-	//QA
-	
+
+	// QA
+
 	public String demo1() {
-		
+
 		return "this is written by QA";
-		
+
 	}
 
-	
-	
-	//developr
+	// developr
 	public String demo2() {
-		
+
 		return "this is developer code";
+	}
+
+	public String demoConflict() {
+
+		return "this is developer conflict code";
 	}
 
 }
